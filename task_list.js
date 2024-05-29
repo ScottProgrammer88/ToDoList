d9b830073aa65bf3c6de53a8f75fe69fc855950c
Scott <greatvstech@gmail.com>
2024-05-29 09:05:19 -0700

    Added modal display for task details and added some CSS styling

--- task_list.js
+++ task_list.js
@@ -4,10 +4,10 @@ const setCookie = (name, value, days) => {
         cookie += "; max-age=" + days * 24 * 60 * 60;
     }
     cookie += "; path=/";
-    document.cookie = cookie; 
+    document.cookie = cookie;
 };
 
-const getCookieByName = name => { 
+const getCookieByName = name => {
     const cookies = document.cookie;
     let start = cookies.indexOf(name + "=");
     if (start === -1) { // no cookie with that name
@@ -18,16 +18,15 @@ const getCookieByName = name => {
         if (end === -1) { // if no semicolon, last cookie
             end = cookies.length;
         }
-        const cookieValue = cookies.substring(start, end); 
+        const cookieValue = cookies.substring(start, end);
         return decodeURIComponent(cookieValue);
     }
 };
 
-const deleteCookie = name => document.cookie = name + "=''; max-age=0; path=/"; 
+const deleteCookie = name => document.cookie = name + "=''; max-age=0; path=/";
 
 $(document).ready(() => {
     $("#add_task").click(() => {
-
         const task = $("#task").val().trim();  // textbox = $("#task"); const task = textbox.val(); 
         if (task === "") {                     // https://www.w3schools.com/jquery/jquery_dom_set.asp
             alert("Please enter a task.");
@@ -38,19 +37,16 @@ $(document).ready(() => {
         tasks += task + "\n";                  // https://www.w3schools.com/jquery/jquery_selectors.asp
         setCookie("tasks", tasks, 21);         // 21 day persistent cookies
         displayTasks();                        // textbox.val("");
-        $("#task").val("").focus();            // $("#task_list").val(getCookieByName("tasks"));
-                                               // textbox.focus();
+        $("#task").val("").focus();            // $("#task_list").val(getCookieByName("tasks")); // textbox.focus()
     });
 
     $("#clear_tasks").click(() => {
         deleteCookie("tasks");
-        displayTasks();
-        // $("#task_list").val("");
-        // $("#task").focus();
+        displayTasks(); // $("#task_list").val(""); // $("#task").focus();
     });
 
     // https://www.w3schools.com/jquery/jquery_traversing_ancestors.asp
-    $("#task_list").on("change", "input[type='checkbox']", function() {
+    $("#task_list").on("change", "input[type='checkbox']", function () {
         const taskIndex = $(this).parent().index(); // Get index of the task in the list
         const tasks = getCookieByName("tasks").split("\n"); // Get tasks from cookie
         tasks[taskIndex] = $(this).is(":checked") ? "✔" + tasks[taskIndex] : tasks[taskIndex].substring(1); // Update task with checkbox state
@@ -58,19 +54,35 @@ $(document).ready(() => {
         displayTasks(); // Refresh task list
     });
 
+    // https://www.w3schools.com/JS/js_events.asp   https://dev.to/ediomojose/pop-up-modal-using-html-css-and-javascript-to-create-a-modal-19gd
+
+    // click event handler for tasks
+    $("#task_list").on("click", ".task-item", function () {
+        const taskText = $(this).text().trim();
+        $("#modal-task-text").text(taskText);
+        $("#task-details-modal").show();
+    });
+
+    // this is to close the button click handler
+    $("#close-modal").click(function () {
+        $("#task-details-modal").hide();
+    });
+
     // Display tasks on initial load
     displayTasks(); // $("#task_list").val(getCookieByName("tasks"));
-                    // $("#task").focus();
+    // $("#task").focus();
 });
+
+
 
 function displayTasks() {
     const tasks = getCookieByName("tasks");
     if (tasks) {
-        const tasksArray = tasks.split("\n");
+        const tasksArray = tasks.split("\n").filter(task => task.trim() !== "");
         const taskList = $("#task_list");
         taskList.empty(); // this clears previous tasks
         tasksArray.forEach(task => {
-            const taskElement = $("<div>"); // .text(task.substring(1)); // this removes check box character
+            const taskElement = $("<div>").addClass("task-item"); // .text(task.substring(1)); // this removes check box character
             const checkbox = $("<input>").attr("type", "checkbox");
             if (task.startsWith("✔")) { // const checkMark = "\u2714";
                 checkbox.prop("checked", true); // Check the checkbox if task is completed
@@ -78,7 +90,8 @@ function displayTasks() {
                 task = task.substring(1);
             }
             taskElement.append(checkbox);
-            taskElement.append(task);
+
+            taskElement.append($("<span>").text(task));
             taskList.append(taskElement);  // https://www.w3schools.com/jquery/html_append.asp
         });
     }
