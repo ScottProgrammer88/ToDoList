fdf8e2bb01508db8d6df97933e7e5bb6533931c0
Scott <greatvstech@gmail.com>
2024-05-28 21:11:47 -0700

    Added checkboxes display for tasks

--- task_list.js
+++ task_list.js
@@ -27,19 +27,19 @@ const deleteCookie = name => document.cookie = name + "=''; max-age=0; path=/";
 
 $(document).ready(() => {
     $("#add_task").click(() => {
-        const textbox = $("#task");
-        const task = textbox.val(); 
-        if (task == "") {
+
+        const task = $("#task").val().trim();  // textbox = $("#task"); const task = textbox.val(); 
+        if (task == "") {                      // https://www.w3schools.com/jquery/jquery_dom_set.asp
             alert("Please enter a task.");
-            textbox.focus();
-        } else {
-            let tasks = getCookieByName("tasks");
-            tasks += task + "\n"; 
-            setCookie("tasks", tasks, 21); // 21 day persistent cookies
-            textbox.val("");
-            $("#task_list").val(getCookieByName("tasks"));
-            textbox.focus();
-        }
+            $("#task").focus();                // textbox.focus(); 
+            return;
+        }                                      // } else {
+        let tasks = getCookieByName("tasks") || "";
+        tasks += task + "\n";                  // https://www.w3schools.com/jquery/jquery_selectors.asp
+        setCookie("tasks", tasks, 21);         // 21 day persistent cookies
+        displayTasks();                        // textbox.val("");
+        $("#task").val("").focus();            // $("#task_list").val(getCookieByName("tasks"));
+                                               // textbox.focus();
     });
 
     $("#clear_tasks").click(() => {
@@ -48,7 +48,32 @@ $(document).ready(() => {
         $("#task").focus();
     });
 
+    $("#task_list").on("change", "input[type='checkbox']", function() {
+        const taskIndex = $(this).parent().index(); // Get index of the task in the list
+        const tasks = getCookieByName("tasks").split("\n"); // Get tasks from cookie
+        tasks[taskIndex] = $(this).is(":checked") ? "✔" + tasks[taskIndex] : tasks[taskIndex].substring(1); // Update task with checkbox state
+        setCookie("tasks", tasks.join("\n"), 21); // Save updated tasks to cookie
+        displayTasks(); // Refresh task list
+    });
+
     // Display tasks on initial load
-    $("#task_list").val(getCookieByName("tasks"));
-    $("#task").focus();
+    displayTasks(); // $("#task_list").val(getCookieByName("tasks"));
+                    // $("#task").focus();
 });
+
+function displayTasks() {
+    const tasks = getCookieByName("tasks");
+    if (tasks) {
+        const tasksArray = tasks.split("\n");
+        $("#task_list").empty(); // this clears previous tasks
+        tasksArray.forEach(task => {
+            const taskElement = $("<div>").text(task.substring(1)); // this removes check box character
+            const checkbox = $("<input>").attr("type", "checkbox");
+            if (task.startsWith("✔")) // const checkMark = "\u2714";
+            checkbox.prop("checked", true); // Check the checkbox if task is completed
+            taskElement.addClass("completed"); // Add completed class to the task
+            taskElement.prepend(checkbox);
+            $("#task_list").append(taskElement);  // https://www.w3schools.com/jquery/html_append.asp
+        });
+    }
+}
